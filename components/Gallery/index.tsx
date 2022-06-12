b56f0de4b7af007a38879924c7e6c5502f791e92
MichalDaniluk <forxik@gmail.com>
2022-06-12 23:41:03 +0200

    Added gallery page

--- components/Gallery/index.tsx
+++ components/Gallery/index.tsx
@@ -10,7 +10,7 @@ const Gallery = () => {
 <>
 <div id="gallery" className="text-center block mt-12 drop-shadow-md">
 	<Header2 title="Gabinety Europejskiego Centrum Rehabilitacji i Psychologii" dark={true} />
-	<SubHeader title="Nizej znajdziesz nasze zdjęcia, sprawdź kim jesteśmy zanim do nas przyjdziesz" dark={true} />
+	<SubHeader title="Poniżej znajdziesz nasze zdjęcia, sprawdź kim jesteśmy zanim do nas przyjdziesz" dark={true} />
 </div>
 <div className="grid grid-cols-3 gap-4 p-12 mb-6 m-auto max-w-screen-2xl w-full">
 	<GalleryBox url="/img/port01.jpg" alt="Parking" />
--- components/mainMenu.tsx
+++ components/mainMenu.tsx
@@ -12,7 +12,7 @@ const  MainMenu = () => {
                   <li className="m-4 text-white uppercase"><Link href="/sis">SIS</Link></li>
                   <li className="m-4 text-white uppercase"><Link href="/tecar">Tecar</Link></li>
                   <li className="m-4 text-white uppercase"><Link href="/rodo">RODO</Link></li>
-                  <li className="m-4 text-white uppercase"><Link href="/#gallery">Galeria</Link></li>
+                  <li className="m-4 text-white uppercase"><Link href="/galeria">Galeria</Link></li>
                   <li className="m-4 text-white uppercase"><Link href="#testimonial">Opinie o nas</Link></li>
                   <li className="m-4 text-white uppercase"><Link href="#contact">Kontakt</Link></li>
                   <li className="m-4 text-white uppercase"><Link href="/assets/deklaracja_dostepnosci.pdf">Deklaracja dostępności</Link></li>
